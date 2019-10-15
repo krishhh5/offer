8bcb44639b6b52ef01d94d8eb3080d053e8ae09b
krishhh5 <46667791+krishhh5@users.noreply.github.com>
2019-10-15 20:10:57 +0800

    chore: day 7

---
+++ h/数字在排序数组中出现的次数.java
@@ -0,0 +1,28 @@
+/*
+统计一个数字在排序数组中出现的次数。
+*/
+public class Solution {
+    public int GetNumberOfK(int [] array , int k) {
+       int start = binarySearch(array, k);
+       int end = binarySearch(array, k+1);
+        //如果返回的start越界，也就是在[1,3,3,3,3,4,5],6时会越界
+        //如果查找之后不存在k这个数，则返回0
+        return (array.length == start || array[start] != k) ? 0 : end - start;
+    }
+    //这个二分查找法，允许有重复的数字，返回重复数字最开始出现的位置
+    //如果没有这个数字，则返回比他大的一个数字的位置
+    private int binarySearch(int [] array, int k)
+    {
+        int start = 0;
+        int end = array.length;
+        while (start < end) {
+            int mid = (end + start) >> 1;
+            if(array[mid] >= k) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
---
+++ h/第一个出现一次的字符.java
@@ -0,0 +1,25 @@
+/*
+在一个字符串(0<=字符串长度<=10000，
+全部由字母组成)中找到第一个只出现一次的字符,
+并返回它的位置, 如果没有则返回 -1（需要区分大小写）.
+*/
+public class Solution {
+    public int FirstNotRepeatingChar(String str) {
+        if(str==null||str.length()==0){
+            return -1;
+        }
+        char [] c = str.toCharArray();
+        int [] counter = new int[124];
+        for (char ch : c) {
+            counter[ch]++;
+        }
+        
+        for (int i = 0; i < 124; i++) {
+            if (counter[c[i]] == 1) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
